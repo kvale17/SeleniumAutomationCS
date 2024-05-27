168f5759f828f23ebcb6c8ca7cd754751cd0502b
kvale17 <kevin.valencia24@gmail.com>
2024-05-26 20:33:00 -0700

    Add UIMethods class

---
+++ SeleniumAutomation/Common/UIMethods.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumCSAutomation.Common
+{
+    public static class UIMethods
+    {
+        private static readonly IWebDriver Driver = BaseTest.Driver;
+
+        public static void AssertText(string cssPath, string expectedText)
+        {
+            string elementText = Driver.FindElement(By.CssSelector(cssPath)).Text;
+
+            Assert.Equal(elementText, expectedText);
+        }
+
+        public static void Click(string cssPath)
+        {
+            Driver.FindElement(By.CssSelector(cssPath)).Click();
+        }
+
+        public static void Type(string cssPath, string text)
+        {
+            IWebElement element = Driver.FindElement(By.CssSelector(cssPath));
+            element.Clear();
+            element.SendKeys(text);
+        }
+    }
+}
