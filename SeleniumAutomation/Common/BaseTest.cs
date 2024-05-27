15a327825136c9b7d66dcc10366d359d01ff035e
kvale17 <kevin.valencia24@gmail.com>
2024-05-26 19:01:37 -0700

    Add BaseTest class

---
+++ SeleniumAutomation/Common/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumCSAutomation.Common
+{
+    public class BaseTest : IDisposable
+    {
+        internal static IWebDriver Driver { get; private set; } = null!;
+
+        //Setup
+        public BaseTest()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        //Teardown
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                Driver.Quit();
+                Driver.Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
